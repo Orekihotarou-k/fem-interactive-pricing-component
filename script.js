322fc9ba32dbed3af1a9f73667d1d95b7723b271
Levis Kim <122298089+Orekihotarou-k@users.noreply.github.com>
2024-02-22 23:21:36 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+// select elements
 const inputslider = document.getElementById("slider")
 const sliderValue = document.querySelector(".sliderValue")
 const toggleButton = document.querySelector(".toggle-button")
 
-
+// change the value of the slider when the range is moved
 slider.oninput = () => {
     let value = inputslider.value
     sliderValue.textContent = "$" + value + ".00"   
 }
 
+// change the value of the slider on mobile devices
+slider.addEventListener("touchmove", () => {
+    let value = slider.value
+    sliderValue.textContent = "$" + value + ".00"
+})
+
+// switch the toggle button
 toggleButton.addEventListener("click", () => {
     toggleButton.classList.toggle("active")
 })
